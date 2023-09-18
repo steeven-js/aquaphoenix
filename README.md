d15b7ef5298e0917d3338c5d857be58020895536
steeven-js <jacques.steeven@gmail.com>
2023-09-18 13:10:27 -0400

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## Aquaphoenix
+
+Génération de bon de livraison et autres
+
+![Aquaphoenix - Dark Mode](https://firebasestorage.googleapis.com/v0/b/dashboard-f3222.appspot.com/o/Tableau%20de%20bord%20-%20Aquaphoenix1.png?alt=media&token=77a0ec6b-49d2-4b88-8f2b-009c22b37f86)
+
+![Aquaphoenix - Light Mode](https://firebasestorage.googleapis.com/v0/b/dashboard-f3222.appspot.com/o/Tableau%20de%20bord%20-%20Aquaphoenix2.png?alt=media&token=a81f2292-a680-418f-a85e-530dda6b6662)
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,7 +36,7 @@ class AdminPanelProvider extends PanelProvider
                 'warning' => Color::Orange,
             ])
             ->sidebarCollapsibleOnDesktop()
-            ->favicon(asset('favicon.ico'))
+            ->favicon(asset('images/favicon.ico'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
