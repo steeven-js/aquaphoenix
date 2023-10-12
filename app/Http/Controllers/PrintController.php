409d251b099385cb1813cc487755a7474d4f547e
steeven-js <jacques.steeven@gmail.com>
2023-10-12 18:50:33 -0400

    wip

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -14,93 +14,15 @@ class PrintController extends Controller
 {
     public function printOrder($id)
     {
-        $order = $this->getOrderById($id);
-        $year = $this->getYearFromOrder($order);
-
-        $orderData = $this->getOrderData($order);
-        $totalQuantity = $this->getTotalQuantity($orderData);
-
-        $pdf = $this->generateOrderPDF($order, $orderData, $year);
-
-        $pdfFileName = $this->getPDFFileName($order);
-        $pdfPath = $this->savePDFAndGetPath($pdf, $pdfFileName, $year);
-        $url = $this->getUrlForPDF($pdfFileName, $year);
-
-        $this->updateOrderURL($order, $url);
-
-        return $pdf->stream($pdfFileName);
-    }
-
-    public function livraison($id)
-    {
-        $order = $this->getOrderById($id);
-        $year = $this->getYearFromOrder($order);
-
-        $orderData = $this->getOrderData($order);
-        $totalQuantity = $this->getTotalQuantity($orderData);
-
-        $pdf = $this->generateOrderPDF($order, $orderData, $year);
-
-        $pdfFileName = $this->getPDFFileName($order);
-        $pdfPath = $this->savePDFAndGetPath($pdf, $pdfFileName, $year);
-        $url = $this->getUrlForPDF($pdfFileName, $year);
-
-        $storage = Storage::url('pdf/' . $year . '/' . 'rapport-livraison' . '/' . $pdfFileName);
-
-        $this->updateOrderURL($order, $url);
-
-        return [
-            'pdfPath' => $pdfPath,
-            'storage' => $storage,
-            'url' => $url,
-        ];
-    }
-
-    public function ordersByMonth($month, $year)
-    {
-        $orders = $this->getOrdersByMonthAndYear($month, $year);
-
-        $ordersData = $this->getOrdersData($orders);
-        $totalQuantity = $this->getMonthTotalQuantity($ordersData);
-
-        $monthName = $this->translateToFrench($month, 'month');
-
-        $MonthTable = Month::where('month_number', $month)->where('year', $year)->first();
-
-        $pdf = $this->generateOrdersByMonthPDF($monthName, $year, $ordersData, $totalQuantity);
-
-        $pdfFileName = $this->getOrdersByMonthPDFFileName($month, $year);
-        $pdfPath = $this->savePDFAndGetPath($pdf, $pdfFileName, $year);
-
-        $this->sendNotificationForMonthReport($monthName, $MonthTable, $year);
-
-        return $pdf->stream('orders_by_month.pdf');
-    }
-
-    // Helper functions
-    private function getOrderById($id)
-    {
         $order = Order::with('customer', 'items')->find($id);
 
         if (!$order) {
             abort(404);
         }
 
-        return $order;
-    }
-
-    private function getYearFromOrder($order)
-    {
-        if (!empty($order->published_at)) {
-            return Carbon::parse($order->published_at)->format('Y');
-        }
-
-        dd('Published date is empty.');
-    }
-
-    private function getOrderData($order)
-    {
+        $year = Carbon::parse($order->published_at)->format('Y');
         $orderData = [];
+
         foreach ($order->items as $item) {
             $product = Product::find($item->product_id);
             if ($product) {
@@ -113,91 +35,116 @@ class PrintController extends Controller
             }
         }
 
-        return $orderData;
-    }
-
-    private function getTotalQuantity($orderData)
-    {
         $totalQuantity = 0;
+
         foreach ($orderData as $item) {
-            $totalQuantity = $totalQuantity + $item['Quantity'];
+            $totalQuantity += $item['Quantity'];
         }
-        return $totalQuantity;
 
-    }
-
-    private function getMonthTotalQuantity($orderData)
-    {
-        $totalQuantity = 0;
-
-        foreach ($orderData as $order) {
-            foreach ($order['Order Items'] as $item) {
-                if (isset($item['Quantity'])) {
-                    $totalQuantity += $item['Quantity'];
-                }
-            }
-        }
-        return $totalQuantity;
-    }
-
-    private function generateOrderPDF($order, $orderData, $year)
-    {
         $formattedCreationDate = $order->getFormattedPublishedDate();
-
         $formattedDeliveredDate = $order->getFormattedDeliveredDate();
 
-        $totalQuantity = $this->getTotalQuantity($orderData);
-
-        return PDF::loadView('pages.rapport.pdf.livraison', compact('order', 'orderData', 'formattedCreationDate', 'formattedDeliveredDate', 'totalQuantity'));
-    }
-
-    private function getPDFFileName($order)
-    {
-        return $order->id . '-' . $order->created_at->format('d-m-Y') . '-' . $order->customer->id . '.pdf';
-    }
-
-    private function savePDFAndGetPath($pdf, $pdfFileName, $year)
-    {
-        $pdfDirectory = 'pdf/' . $year . '/' . 'rapport-livraison' . '/';
-        $fullPdfDirectory = storage_path('app/public/' . $pdfDirectory);
+        $pdf = PDF::loadView('pages.rapport.pdf.livraison', compact('order', 'orderData', 'formattedCreationDate', 'formattedDeliveredDate', 'totalQuantity'));
+        $pdfFileName = $order->id . '-' . $order->created_at->format('d-m-Y') . '-' . $order->customer->id . '.pdf';
+        $pdfDirectory = "pdf/{$year}/rapport-livraison/";
+        $fullPdfDirectory = storage_path("app/public/{$pdfDirectory}");
 
         if (!file_exists($fullPdfDirectory)) {
             mkdir($fullPdfDirectory, 0755, true);
         }
 
         $pdfPath = $fullPdfDirectory . $pdfFileName;
-
         $pdf->save($pdfPath);
+        $url = env('APP_URL') . Storage::url("pdf/{$year}/rapport-livraison/{$pdfFileName}");
 
-        return $pdfPath;
-    }
-
-    private function getUrlForPDF($pdfFileName, $year)
-    {
-        $app_url = env('APP_URL');
-        $storage = Storage::url('pdf/' . $year . '/' . 'rapport-livraison' . '/' . $pdfFileName);
-        return $app_url . $storage;
-    }
-
-    private function updateOrderURL($order, $url)
-    {
         $order->url = $url;
         $order->save();
+
+        $mailData = [
+            'order' => $order,
+            'number' => $order->number,
+            'pdfPath' => $pdfPath,
+            'orderUrl' => route('order.print', $order->id),
+            'url' => $url,
+            'formattedCreationDate' => $order->getFormattedPublishedDate(),
+            'formattedDeliveredDate' => $order->getFormattedDeliveredDate(),
+            'totalQuantity' => $totalQuantity,
+        ];
+
+        $this->sendEmail($mailData);
+
+        return $pdf->stream($pdfFileName);
     }
 
-    private function getOrdersByMonthAndYear($month, $year)
+    public function livraison($id)
     {
-        return Order::with('customer', 'items')
+        $order = Order::with('customer', 'items')->find($id);
+
+        if (!$order) {
+            abort(404);
+        }
+
+        $year = Carbon::parse($order->published_at)->format('Y');
+        $orderData = [];
+
+        foreach ($order->items as $item) {
+            $product = Product::find($item->product_id);
+            if ($product) {
+                $orderData[] = [
+                    'product_id' => $item->product_id,
+                    'Product Name' => $product->name,
+                    'Description' => $product->description,
+                    'Quantity' => $item->qty,
+                ];
+            }
+        }
+
+        $totalQuantity = 0;
+
+        foreach ($orderData as $item) {
+            $totalQuantity += $item['Quantity'];
+        }
+
+        $formattedCreationDate = $order->getFormattedPublishedDate();
+        $formattedDeliveredDate = $order->getFormattedDeliveredDate();
+
+        $pdf = PDF::loadView('pages.rapport.pdf.livraison', compact('order', 'orderData', 'formattedCreationDate', 'formattedDeliveredDate', 'totalQuantity'));
+        $pdfFileName = $order->id . '-' . $order->created_at->format('d-m-Y') . '-' . $order->customer->id . '.pdf';
+        $pdfDirectory = "pdf/{$year}/rapport-livraison/";
+        $fullPdfDirectory = storage_path("app/public/{$pdfDirectory}");
+
+        if (!file_exists($fullPdfDirectory)) {
+            mkdir($fullPdfDirectory, 0755, true);
+        }
+
+        $pdfPath = $fullPdfDirectory . $pdfFileName;
+        $pdf->save($pdfPath);
+        $url = env('APP_URL') . Storage::url("pdf/{$year}/rapport-livraison/{$pdfFileName}");
+
+        $order->url = $url;
+        $order->save();
+
+        $storage = Storage::url("pdf/{$year}/rapport-livraison/{$pdfFileName}");
+
+        return [
+            'pdfPath' => $pdfPath,
+            'storage' => $storage,
+            'url' => $url,
+        ];
+    }
+
+    public function ordersByMonth($month, $year)
+    {
+        $orders = Order::with('customer', 'items')
             ->whereMonth('published_at', $month)
             ->whereYear('published_at', $year)
             ->get();
-    }
 
-    private function getOrdersData($orders)
-    {
         $ordersData = [];
+
         foreach ($orders as $order) {
             $orderData = [];
+
             foreach ($order->items as $item) {
                 $product = Product::find($item->product_id);
                 if ($product) {
@@ -209,8 +156,10 @@ class PrintController extends Controller
                     ];
                 }
             }
+
             $formattedCreationDate = $order->getFormattedPublishedDate();
             $formattedDeliveredDate = $order->getFormattedDeliveredDate();
+
             $ordersData[] = [
                 'Order ID' => $order->id,
                 'Order Number' => $order->number,
@@ -220,31 +169,41 @@ class PrintController extends Controller
                 'Order Items' => $orderData,
             ];
         }
-        return $ordersData;
-    }
 
-    private function generateOrdersByMonthPDF($monthName, $year, $ordersData, $totalQuantity)
-    {
-        return PDF::loadView('pages.rapport.pdf.orders_by_month', [
+        $totalQuantity = 0;
+
+        foreach ($ordersData as $order) {
+            foreach ($order['Order Items'] as $item) {
+                if (isset($item['Quantity'])) {
+                    $totalQuantity += $item['Quantity'];
+                }
+            }
+        }
+
+        $monthName = $this->translateToFrench($month, 'month');
+        $MonthTable = Month::where('month_number', $month)->where('year', $year)->first();
+
+        $pdf = PDF::loadView('pages.rapport.pdf.orders_by_month', [
             'monthName' => $monthName,
             'year' => $year,
             'ordersData' => $ordersData,
             'totalQuantity' => $totalQuantity,
         ]);
-    }
 
-    private function getOrdersByMonthPDFFileName($month, $year)
-    {
-        return $month . '-' . $year . '-' . '.pdf';
-    }
+        $pdfFileName = $month . '-' . $year . '.pdf';
+        $pdfDirectory = "pdf/{$year}/rapport-livraison/";
+        $fullPdfDirectory = storage_path("app/public/{$pdfDirectory}");
 
-    private function sendNotificationForMonthReport($monthName, $MonthTable, $year)
-    {
-        Notification::make()
-            ->title('Nouvelle commande')
-            ->icon('heroicon-o-shopping-bag')
-            ->body("** Rapport du mois {$monthName} créé le {$MonthTable->report_created_at} **")
-            ->sendToDatabase(auth()->user());
+        if (!file_exists($fullPdfDirectory)) {
+            mkdir($fullPdfDirectory, 0755, true);
+        }
+
+        $pdfPath = $fullPdfDirectory . $pdfFileName;
+        $pdf->save($pdfPath);
+
+        $this->sendNotificationForMonthReport($monthName, $MonthTable, $year);
+
+        return $pdf->stream('orders_by_month.pdf');
     }
 
     public function translateToFrench($value, $type)
@@ -281,5 +240,16 @@ class PrintController extends Controller
         }
 
         return ''; // Si le type n'est pas pris en charge, retourne une chaîne vide
+    }
+
+    private function sendEmail($mailData)
+    {
+        $pdfPath = $result['pdfPath'];
+        $storage = $result['storage'];
+        $url = $result['url'];
+
+        Mail::to('lianajacques18@gmail.com')
+            ->cc(['liana.jacques@aquaphoenix.fr', 'jacques.steeven@gmail.com'])
+            ->send(new LivraisonMail($mailData, $storage, $url));
     }
 }
