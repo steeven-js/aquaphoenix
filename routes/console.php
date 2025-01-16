192b1c235d85919f80b6dfb17b701e43912f34f2
steeven-js <jacques.steeven@gmail.com>
2025-01-16 18:23:49 -0400

    Add 'stats:initialize' command to console routes for monthly statistics initialization

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Artisan::command('stats:initialize', function () {
+    $this->call('stats:initialize');
+})->purpose('Initialise les statistiques mensuelles')->everyMinute();
