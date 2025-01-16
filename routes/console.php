54f727004658fd73475c83b34582eb25030fc5b4
steeven-js <jacques.steeven@gmail.com>
2025-01-16 18:50:54 -0400

    Refactor 'stats:initialize' command to use Schedule for improved clarity and organization
    - Replaced the Artisan command definition for 'stats:initialize' in console.php with a scheduled command using the Schedule facade.
    - This change enhances the command structure by streamlining the scheduling process and maintaining a clean codebase.

--- routes/console.php
+++ routes/console.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
 
-Artisan::command('stats:initialize', function () {
-    $this->call('stats:initialize');
-})->purpose('Initialise les statistiques mensuelles')->everyMinute();
+Schedule::command('stats:initialize')->everyMinute();
+
