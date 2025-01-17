ed6833fb9c168872bbe4fec9829f8deb494404ae
steeven-js <jacques.steeven@gmail.com>
2025-01-16 21:00:32 -0400

    everyThirtyMinutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('stats:initialize')
-            ->everyMinute();
+            ->everyThirtyMinutes();
     }
 
     /**
--- routes/console.php
+++ routes/console.php
@@ -9,5 +9,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 
-Schedule::command('stats:initialize')->everyMinute();
+Schedule::command('stats:initialize')->everyThirtyMinutes();
 
